b54bf8ff279b5c30d9596a4bfec5f3eabd3ca92b
sapprama <saptarshi.pramanick@oracle.com>
2023-09-12 22:51:54 +0530

    The webhook 5

--- src/main/java/com/psja/DockerTest/api/ApplicationController.java
+++ src/main/java/com/psja/DockerTest/api/ApplicationController.java
@@ -16,7 +16,7 @@ public class ApplicationController {
 	
 	@RequestMapping(value = "/getData")
 	public ResponseEntity<String> getData(){
-		logger.info( "Hi i am inside method8" );
+		logger.info( "Hi i am inside method9" );
 		return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.TEXT_PLAIN).body( "HII PSJA TEST" );
 	}
 	
